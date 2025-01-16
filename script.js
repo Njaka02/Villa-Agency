e9e883cdfbb143037bf32cf38e3a0500087323f8
Njaka02 <njakaharilantoandriamijeri@gmail.com>
2025-01-16 09:52:02 +0300

    [ADD] JavaScript for button and files HTML and CSS

--- script.js
+++ script.js
@@ -24,3 +24,71 @@ swiper.on("slideChange", () => {
     const currentIndex = swiper.activeIndex;
     sliderTabs[currentIndex].checked = true;
 });
+
+
+const buttonInfo = document.querySelectorAll(".button-info");
+const information = document.querySelectorAll(".information");
+
+buttonInfo.forEach((button, index) => {
+    button.addEventListener("click", () => {
+        information.forEach((info, i) => {
+            if (i !== index) {
+                info.classList.remove("active");
+            }
+        });
+        information[index].classList.toggle("active");
+    });
+});
+
+
+function changeColor(button) {
+    if (button.classList.contains('clicked')) {
+      button.classList.remove('clicked');
+    } else {
+      const buttons = document.querySelectorAll('.button-info');
+      buttons.forEach((btn) => {
+        btn.classList.remove('clicked');
+      });  
+      button.classList.add('clicked');
+    }
+  }
+    document.querySelectorAll('.button-info').forEach((button) => {
+    button.addEventListener('click', () => {
+      changeColor(button);
+    });
+  });
+  
+
+const Appartment = document.querySelectorAll(".Appartment");
+const Deal = document.querySelectorAll(".deal-01");
+
+  if (Deal.length > 0) {
+    Deal[0].classList.add("active");
+  }
+  Appartment.forEach((button, index) => {
+      button.addEventListener("click", () => {
+        Deal.forEach((info, i) => {
+              if (i !== index) {
+                  info.classList.remove("active");
+              }
+          });
+          Deal[index].classList.add("active");
+      });
+  });
+  
+  function changeColor(button) {
+    if (button.classList.contains('clicked')) {
+      button.classList.remove('clicked');
+    } else {
+      const buttons = document.querySelectorAll('.Appartment');
+      buttons.forEach((btn) => {
+        btn.classList.remove('clicked');
+      });  
+      button.classList.add('clicked');
+    }
+  }
+    document.querySelectorAll('.Appartment').forEach((button) => {
+    button.addEventListener('click', () => {
+      changeColor(button);
+    });
+  });
